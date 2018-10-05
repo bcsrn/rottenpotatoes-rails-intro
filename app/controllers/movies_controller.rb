cee7747d0815128798c6d1cca63d7e3596066259
EC2 Default User <ec2-user@ip-172-31-43-3.us-east-2.compute.internal>
2018-10-05 17:47:47 +0000

    changedversion1.1replaced params with ratings

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -42,9 +42,9 @@ class MoviesController < ApplicationController
       end
     end
     if condition == nil
-      @movies = Movie.where("rating IN (?)", params[:ratings].keys)
+      @movies = Movie.where("rating IN (?)", @ratings.keys)
     else
-      @movies = Movie.where("rating IN (?)", params[:ratings].keys)
+      @movies = Movie.where("rating IN (?)", @ratings.keys)
     end   
 
     # change color of table when sorting is finished
